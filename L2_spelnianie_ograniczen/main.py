519f4121673080e5df3911fa4470890a2f884987
Kaja Limisiewicz <k.limisiewicz@gmail.com>
2021-03-29 02:12:26 +0200

    feat: CSP: map coloring

--- L2_spelnianie_ograniczen/main.py
+++ L2_spelnianie_ograniczen/main.py
@@ -1,6 +1,15 @@
+import objects as obj
+
+NODE_DOMAIN_3 = ['red', 'yellow', 'blue']
+NODE_DOMAIN_4 = ['red', 'yellow', 'blue', 'purple']
+HEURISTICS = ['most-constrained-variable', 'most-constraining-variable']
+
 
 def main():
-    pass
+    zad1 = obj.Map(8, NODE_DOMAIN_4)
+    csp = obj.CSP(0, zad1)
+    csp.backtracking()
+    zad1.show_graph()
 
 
 if __name__ == '__main__':
